f015957a9d81583483f35799629579dbea935a23
ratrun <ratrun@gmx.at>
2016-04-03 10:14:41 +0200

    Adding of about menu.

--- web/src/main/webapp/js/menu.js
+++ web/src/main/webapp/js/menu.js
@@ -15,12 +15,11 @@ function webkitapp()
         })
     );
 
-    // Create a sub-menu
+    // Create the osm data sub-menu
     var osmDataSubMenu = new gui.Menu();
     osmDataSubMenu.append(new gui.MenuItem({ label: 'Show available OSM data files' }));
     osmDataSubMenu.append(new gui.MenuItem({ label: 'Download new OSM data file' }));
     osmDataSubMenu.append(new gui.MenuItem({ label: 'Delete OSM data file' }));
-    osmDataSubMenu.append(new gui.MenuItem({ label: 'Create marker data' }));
 
     menu.append(
         new gui.MenuItem({
@@ -30,12 +29,11 @@ function webkitapp()
     );
 
     var graphhopperSubMenu = new gui.Menu();
-    graphhopperSubMenu.append(new gui.MenuItem({ label: 'Change active graph: None' ,  enabled : false} ));
+    graphhopperSubMenu.append(new gui.MenuItem({ label: 'Change active graph' ,  enabled : false} ));
     graphhopperSubMenu.append(new gui.MenuItem({ type: 'separator' ,  enabled : false }))
-    graphhopperSubMenu.append(new gui.MenuItem({ label: 'change graph settings' }));
-    graphhopperSubMenu.append(new gui.MenuItem({ label: 'Create graph' ,  enabled : false}));
-    
-        
+    graphhopperSubMenu.append(new gui.MenuItem({ label: 'Change graph settings' }));
+    graphhopperSubMenu.append(new gui.MenuItem({ label: 'Calculate graph' ,  enabled : false}));
+
     menu.append(
         new gui.MenuItem({
             label: 'Graphhopper',
@@ -43,11 +41,22 @@ function webkitapp()
         })
     );
 
+    // Create the about sub-menu
+    var aboutSubMenu = new gui.Menu();
+    aboutSubMenu.append(new gui.MenuItem({ label: 'Active graph: None' ,  enabled : false} ));
+
+    menu.append(
+        new gui.MenuItem({
+            label: 'About',
+            submenu: aboutSubMenu
+        })
+    );
+
         // Append Menu to Window
     gui.Window.get().menu = menu;
 }
 
-// Test if we are running under nw. If so the window.require('nw.gui'); command succeeds, otherwise we get an exception
+// Test if we are running under nwjs. If so the window.require('nw.gui'); command succeeds, otherwise we get an exception
 try {
      var gui = window.require('nw.gui');
      webkitapp();
