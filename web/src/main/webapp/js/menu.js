7161ec6c60e1dac17fb3e7fb8f8450a399580eb5
ratrun <ratrun@gmx.at>
2016-05-31 18:53:11 +0200

    mbtilesFileDialog and osmFileDialog for download Implementation of map selection dialog for download.

--- web/src/main/webapp/js/menu.js
+++ web/src/main/webapp/js/menu.js
@@ -5,14 +5,21 @@ var stopTileServerMenuItem;
 var startTileServerMenuItem;
 var stopGraphhopperServerMenuItem;
 var startGraphhopperServerMenuItem;
+var showInstalledMapsMenuItem;
+var changeGraphMenuItem;
+var deleteMapMenuItem;
 
 var aboutWindow = null;
+var selectCountryWindow = null;
 var tilesServerHasExited = true;
 var graphhopperServerHasExited = true;
 var shutdownapp = false;
 var fs;
 var notifier;
-var activeOsmfile = 'liechtenstein-latest.osm.pbf';
+var activeOsmfile = localStorage.activeOsmfile;
+
+if (activeOsmfile === undefined)
+    activeOsmfile = 'liechtenstein-latest.osm.pbf';
 
 function startLocalVectorTileServer(win)
 {
@@ -23,12 +30,13 @@ function startLocalVectorTileServer(win)
        cwd: 'ratrun-mbtiles-server',
        detached: false
     });
-    
+
     stopTileServerMenuItem.enabled = true;
+    showInstalledMapsMenuItem.enabled = true;
     startTileServerMenuItem.enabled = false;
+    deleteMapMenuItem.enabled = false;
 
     console.log('mbtiles started: ' + mbtiles);
-        
     mbtiles.on('error', function (err) {
       console.log('mbtiles error' + err);
     });
@@ -47,6 +55,7 @@ function startLocalVectorTileServer(win)
 
     mbtiles.on('exit', function (code, signal) {
         console.log('tiles server child process exited with code ' + code +' signal=' + signal);
+        showHtmlNotification("./img/mtb.png", 'Tile server stopped !!' , '');
         tilesServerHasExited = true;
         setTimeout(function(){ 
               if (signal === 'SIGTERM')
@@ -55,7 +64,9 @@ function startLocalVectorTileServer(win)
                 if (code === 100) // Code for received stop trigger 
                 {
                     stopTileServerMenuItem.enabled = false;
+                    showInstalledMapsMenuItem.enabled = false;
                     startTileServerMenuItem.enabled = true;
+                    deleteMapMenuItem.enabled = true;
                 }
                 else
                    // We got this most likely during startup because the vector tile server is already active and the 
@@ -97,8 +108,8 @@ function startGraphhopperServer(win)
     console.log('graphhopper started: ' + graphhopper);
     graphhopperServerHasExited = false;
     stopGraphhopperServerMenuItem.enabled = true;
+    changeGraphMenuItem.enabled = false;
     startGraphhopperServerMenuItem.enabled = false;
-    deleteGraphMenuItem.enabled = false;
 
     graphhopper.on('error', function (err) {
       console.log('graphhopper error' + err);
@@ -157,11 +168,23 @@ function startGraphhopperServer(win)
 function stopLocalVectorTileServer()
 {
   if (!tilesServerHasExited)
-  {   // Inform the tile server via SIGTERM to close
+  {
+   if (shutdownapp)
+   {
+      // Inform the tile server via SIGTERM to close
       var res = mbtiles.kill('SIGTERM');
       console.log("mbtiles kill SIGTERM returned:" + res);
-      stopTileServerMenuItem.enabled = false;
-      startTileServerMenuItem.enabled = true;
+   }
+   else
+   {  // Call special shutdown URL
+      $.getJSON("http://127.0.0.1:3000/4cede326-7166-4cbd-994f-699c6dc271e9", function( data ) {
+                             console.log("Tile server stop response was" + data);
+      });
+   }
+   stopTileServerMenuItem.enabled = false;
+   showInstalledMapsMenuItem.enabled = false;
+   startTileServerMenuItem.enabled = true;
+   deleteMapMenuItem.enabled = true;
   }
 }
 
@@ -170,8 +193,8 @@ function stopGraphhopperServer()
   if (!graphhopperServerHasExited)
   {   // Inform the graphhopper server to close
       stopGraphhopperServerMenuItem.enabled = false;
+      changeGraphMenuItem.enabled = true;
       startGraphhopperServerMenuItem.enabled = true;
-      deleteGraphMenuItem.enabled = true;
       var res = graphhopper.kill('SIGTERM');
       console.log("graphhopper kill SIGTERM returned:" + res);
   }
@@ -216,13 +239,13 @@ var download = function(url, dest, cb) {
 function deletegraph(dir)
 {
   if (graphhopperServerHasExited)
-  {
-      fs.unlinkSync(dir + '/edges');
-      fs.unlinkSync(dir + '/geometry');
-      fs.unlinkSync(dir + '/location_index');
-      fs.unlinkSync(dir + '/names');
-      fs.unlinkSync(dir + '/properties');
-      fs.unlinkSync(dir + '/nodes');
+  { console.log('Deleting graph in ' + dir);
+    fs.unlinkSync(dir + '/edges');
+    fs.unlinkSync(dir + '/geometry');
+    fs.unlinkSync(dir + '/location_index');
+    fs.unlinkSync(dir + '/names');
+    fs.unlinkSync(dir + '/properties');
+    fs.unlinkSync(dir + '/nodes');
   }
   else
       alert("Cannot delete graph as graphhopper server is running!");
@@ -238,35 +261,71 @@ function webkitapp(win)
     // Create a sub-menu
     var mapSubMenu = new gui.Menu();
     var separator = new gui.MenuItem({ type: 'separator' , enabled : false });
-    mapSubMenu.append(new gui.MenuItem({ label: 'Show installed maps',
+    // FIXME or delete: alert does not work in case the list gets long, we need a special dialog. But this seems too expensive
+    showInstalledMapsMenuItem = new gui.MenuItem({ label: 'Show installed maps', enabled : tilesServerHasExited,
         click: function() { 
                              $.getJSON("http://127.0.0.1:3000/mbtilesareas.json", function( data ) {
-                                    alert("Installed maps: \n" + JSON.stringify(data).replace(/{\"country\"\:\"/g,'').replace(/\"}/g,''));
+                                    alert("Installed maps: \n" + JSON.stringify(data).replace(/{\"country\"\:\"/g,'').replace(/\"}/g,'\n'));
                              });
                           }
-    }));
-    mapSubMenu.append(new gui.MenuItem({ label: 'Add map: Fixme',
-        click: function() {
-        	     stopLocalVectorTileServer();
-               download("https://osm2vectortiles-downloads.os.zhdk.cloud.switch.ch/v2.0/extracts/liechtenstein.mbtiles", "ratrun-mbtiles-server\\liechtenstein.mbtiles",
-               function(err) {
-                   alert(err);
-               }
-               );
-        }
-    }));
-    mapSubMenu.append(new gui.MenuItem({ label: 'Delete map: Fixme' , enabled : false }));
-    stopTileServerMenuItem = new gui.MenuItem({ label: 'Stop tile server', enabled : tilesServerHasExited ,
-        click: function() { 
-                             console.log("Stop tile server clicked");
-                             $.getJSON("http://127.0.0.1:3000/4cede326-7166-4cbd-994f-699c6dc271e9", function( data ) {
-                                    console.log("Tile server stop response was" + data);
-                             });
-                             startTileServerMenuItem.enabled = true;
-                             stopTileServerMenuItem.enabled = false;
-                           }
     });
-    startTileServerMenuItem = new gui.MenuItem({ label: 'Start tile server', enabled : !tilesServerHasExited , 
+    mapSubMenu.append(showInstalledMapsMenuItem);
+    var country_extracts = global.require('./ratrun-mbtiles-server/country_extracts.json');
+
+    mapSubMenu.append(new gui.MenuItem({ label: 'Download map',
+        click: function() {
+                            var dialog;
+                            var selected_country_file_name;
+
+                            $('#mapDropDownDest').change(function () {
+                                            selected_country_file_name = $(this).val();
+                                        });
+
+                            $(function () {
+                                dialog = $("#map_selection_dialog").dialog({
+                                    width: 420,
+                                    height: 260,
+                                    autoOpen: false,
+                                    resizable: false,
+                                    draggable: false,
+                                    buttons: {
+                                        "Download": function () {
+                                        	  stopLocalVectorTileServer();
+                                            showHtmlNotification("./img/mtb.png", 'Starting download of' , selected_country_file_name);
+                                            download("https://osm2vectortiles-downloads.os.zhdk.cloud.switch.ch/v2.0/extracts/" + selected_country_file_name + ".mbtiles", "ratrun-mbtiles-server\\" + selected_country_file_name + ".mbtiles",
+                                            function(result) {
+                                               showHtmlNotification("./img/mtb.png", 'Download result:', result);
+                                               startLocalVectorTileServer(win);
+                                            });
+                                            $(this).dialog("close");
+                                        },
+                                        Cancel: function () {
+                                            $(this).dialog("close");
+                                        }
+                                    }
+                                });
+                           });
+                           $.each(country_extracts, function (key, value) {
+                             // Populate selected_country_file_name with value of first entry
+                             if (selected_country_file_name == undefined)
+                                 selected_country_file_name = value.extract;
+                             console.log('country: '+ value.country );
+                                $('#mapDropDownDest').append($('<option></option>').val(value.extract).html(value.country));
+                           });
+                            $("#map_selection_dialog").dialog('open');
+                          }
+    }));
+
+    deleteMapMenuItem = new gui.MenuItem({ label: 'Delete map', enabled : !tilesServerHasExited,
+        click: function() { stopLocalVectorTileServer();
+                            chooseFile('#mbtilesFileDialog');
+        }
+    });
+    mapSubMenu.append(deleteMapMenuItem);
+    stopTileServerMenuItem = new gui.MenuItem({ label: 'Stop tile server', enabled : tilesServerHasExited,
+        click: function() { stopLocalVectorTileServer(); }
+    });
+    startTileServerMenuItem = new gui.MenuItem({ label: 'Start tile server', enabled : !tilesServerHasExited,
         click: function() { 
                              startLocalVectorTileServer(win);
                              this.enabled = false;
@@ -286,35 +345,33 @@ function webkitapp(win)
     var graphhopperSubMenu = new gui.Menu()
     var win = gui.Window.get();
 
-    stopGraphhopperServerMenuItem = new gui.MenuItem({ label: 'Stop Graphhopper server', enabled : graphhopperServerHasExited , 
+    stopGraphhopperServerMenuItem = new gui.MenuItem({ label: 'Stop Graphhopper server', enabled : graphhopperServerHasExited,
         click: function() { 
                              console.log('Stop Graphhopper server clicked');
                              stopGraphhopperServer();
                            }
     });
-    startGraphhopperServerMenuItem = new gui.MenuItem({ label: 'Start Graphhopper server', enabled : !graphhopperServerHasExited , 
+    startGraphhopperServerMenuItem = new gui.MenuItem({ label: 'Start Graphhopper server', enabled : !graphhopperServerHasExited,
         click: function() {  console.log('Start Graphhopper server clicked');
                              startGraphhopperServer(win);
                              this.enabled = false;
                           }
     });
-    graphhopperSubMenu.append(new gui.MenuItem({ label: 'Change active graph: Fixme' ,  enabled : false} ));
-    graphhopperSubMenu.append(separator);
-    // graphhopperSubMenu.append(new gui.MenuItem({ label: 'Change graph settings', enabled : false }));
-    graphhopperSubMenu.append(new gui.MenuItem({ label: 'Calculate new graph: Fixme' ,  enabled : false}));
-    deleteGraphMenuItem = new gui.MenuItem({ label: 'Delete graph', enabled : !graphhopperServerHasExited , 
-        click: function() {  console.log('Delete graph clicked');
-                             deletegraph('graphhopper/graph');
+    //graphhopperSubMenu.append(new gui.MenuItem({ label: 'Change active graph: Fixme' ,  enabled : false} ));
+    //graphhopperSubMenu.append(separator);
+    //graphhopperSubMenu.append(new gui.MenuItem({ label: 'Change graph settings', enabled : false })); // ?? Needed ??
+    changeGraphMenuItem = new gui.MenuItem({ label: 'Change graph', enabled : !graphhopperServerHasExited,
+        click: function() {
+                             chooseFile('#osmFileDialog');
                           }
     });
-    graphhopperSubMenu.append(deleteGraphMenuItem);
+    graphhopperSubMenu.append(changeGraphMenuItem);
     graphhopperSubMenu.append(separator);
-    graphhopperSubMenu.append(new gui.MenuItem({ label: 'Show available OSM data files: Fixme' , enabled : false }));
-    graphhopperSubMenu.append(new gui.MenuItem({ label: 'Download new OSM data file: Fixme',
+    graphhopperSubMenu.append(new gui.MenuItem({ label: 'Download OSM data file: Fixme',
         click: function() {
             download("https://download.geofabrik.de/europe/liechtenstein-latest.osm.pbf", "graphhopper\\osmfiles\\liechtenstein-latest.osm.pbf",
-               function(err) {
-                   alert(err);
+               function(result) {
+                   alert('Download OSM resulted in:' + result);
                });
         }
     }));
@@ -388,6 +445,36 @@ var showHtmlNotification = function (icon, title, body, callback) {
   });
 };
 
+ function chooseFile(name) {
+    var chooser = $(name);
+    chooser.unbind('change');
+    chooser.change(function(evt) {
+      if (name === '#osmFileDialog')
+      { activeOsmfile = $(this).val().split(/(\\|\/)/g).pop();
+        console.log('Selected OSM file:' + activeOsmfile);
+        localStorage['activeOsmfile'] = activeOsmfile;
+        deletegraph('graphhopper/graph');
+        startGraphhopperServer(win);
+      }
+      else
+      {
+         if(name === '#mbtilesFileDialog')
+         {
+           var deletedFile = $(this).val();
+           fileName = deletedFile.split(/(\\|\/)/g).pop();
+           console.log('fileName:' + fileName);
+           if ( fileName.indexOf('bicycle') === -1)
+           {
+             console.log('Delete map file:' + deletedFile);
+             fs.unlinkSync(deletedFile);
+           }
+           else
+             showHtmlNotification("./img/warning.png", "Avoid deletion of protected file!", fileName);
+         }
+      }
+    });
+    chooser.trigger('click');
+}
 // Test if we are running under nwjs. If so the window.require('nw.gui'); command succeeds, otherwise we get an exception
 try {
      var gui = window.require('nw.gui');
@@ -397,5 +484,4 @@ try {
  catch(err) {
      // Ignore: We are not running under nw
  }
-
 
