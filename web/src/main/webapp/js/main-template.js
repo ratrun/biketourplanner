e1c2d3504fe65b5d9eb7401a1e2b1b4b337dc8e8
ratrun <ratrun@gmx.at>
2016-08-21 09:38:39 +0200

    Adding of none functional triptree using jqtree.

--- web/src/main/webapp/js/main-template.js
+++ web/src/main/webapp/js/main-template.js
@@ -88,7 +88,28 @@ $(document).ready(function (e) {
     $('#locationform').submit(function (e) {
         // no page reload
         e.preventDefault();
-        mySubmit();
+        graphHopperSubmit();
+    });
+
+    $('#tripForm').submit(function (e) {
+        // no page reload
+        e.preventDefault();
+        tripSubmit();
+    });
+
+    $( "#addtripButton" ).click(function(e) {
+         e.preventDefault();
+         alert("addtripButton clicked");
+         return;
+    });
+
+    $( "#storeButton" ).click(function(e) {
+         e.preventDefault();
+         $("#tripDiv").show();
+         $("#changeTripButton").show();
+         $("#addtripButton").show();
+         $("#routingOptions").hide();
+         return;
     });
 
     var urlParams = urlTools.parseUrlWithHisto();
@@ -621,7 +642,7 @@ function routeLatLng(request, doQuery) {
     });
 }
 
-function mySubmit() {
+function graphHopperSubmit() {
     var fromStr,
             toStr,
             viaStr,
@@ -669,6 +690,13 @@ function mySubmit() {
     // route!
     if (inputOk)
         resolveCoords(allStr);
+}
+
+function tripSubmit() {
+    $("#tripDiv").hide();
+    $("#changeTripButton").hide();
+    $("#addtripButton").hide();
+    $("#routingOptions").show();
 }
 
 $(function() {
@@ -763,4 +791,59 @@ function isProduction() {
 }
 
 module.exports.setFlag = setFlag;
+
+$(function() {
+    var $tree = $('#tripTree');
+    var data = [
+        {
+            label: 'Einzeltouren' , id: 1 , 
+            children: [
+                { label: 'Rennrad' , id: 2 ,
+                    children: [
+                        {
+                           name: 'Tulln' , id: 3 
+                        }
+                    ]
+                },
+                { label: 'Mountainbike', id: 4}
+            ]
+        },
+        {
+            label: 'Mehrtagestouren', id: 6,
+            children: [
+                { name: 'Donauradweg', id: 7 }
+            ]
+        }
+    ];
+
+    $tree.tree({
+       data: data,
+       dragAndDrop: true,
+       onCreateLi: function(node, $li) {
+            // Append a link to the jqtree-element div.
+            // The link has an url '#node-[id]' and a data property 'node-id'.
+            $li.find('.jqtree-element').append(
+                '<a href="#node-'+ node.id +'" class="edit" data-node-id="'+
+                node.id +'"> edit</a>;'
+            );
+        }
+    });
+
+    // Handle a click
+    $tree.on(
+        'click', '.edit',
+        function(e) {
+            // Get the id from the 'node-id' data property
+            var node_id = $(e.target).data('node-id');
+
+            // Get the node from the tree
+            var node = $tree.tree('getNodeById', node_id);
+
+            if (node) {
+                // Display the node name
+                alert(node.name);
+            }
+        }
+    );    
+});
 
