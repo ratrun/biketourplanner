a02a15a97f503c753f554215d0b36e39813fca25
ratrun <ratrun@gmx.at>
2016-09-26 18:33:29 +0200

    Removal of export-link, which does not work especially under nwjs, but also otherwise.

--- web/src/main/webapp/js/instructions.js
+++ web/src/main/webapp/js/instructions.js
@@ -89,8 +89,6 @@ module.exports.create = function (mapLayer, path, urlForHistory, request) {
 
     hiddenDiv.append("<span>" + infoStr + "</span>");
 
-    var exportLink = $("#export-link a");
-    exportLink.attr('href', urlForHistory);
     var osmRouteLink = $("<br/><a>view on OSM</a>");
 
     var osmVehicle = "bicycle";
