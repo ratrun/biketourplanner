c5586b2de29e7abbd0c0fbad95517184f66adbff
ratrun <ratrun@gmx.at>
2016-10-08 20:35:13 +0200

    Fix broken scalecontrol when switching between km to miles.
    Conflicts:
    	web/src/main/webapp/js/main.js

--- web/src/main/webapp/js/map.js
+++ web/src/main/webapp/js/map.js
@@ -394,7 +394,7 @@ module.exports.updateScale = function (useMiles) {
     if (scaleControl === null) {
         return;
     }
-    scaleControl.remove();
+    map.removeControl(scaleControl);
     var options = useMiles ? {metric: false} : {imperial: false};
     scaleControl = L.control.scale(options).addTo(map);
 };
