71180a6fff022c3c58c8798a025d025f18b0a803
ratrun <ratrun@gmx.at>
2017-01-12 19:26:14 +0100

    Check in of result files from https://github.com/bug7a/iconselect.js/tree/master Initial handling of POI iconselect by using from https://github.com/bug7a/iconselect.js/tree/master Check in of POI by taking icons from https://github.com/scottdejonge/map-icons

--- web/src/main/webapp/js/map.js
+++ web/src/main/webapp/js/map.js
@@ -126,7 +126,7 @@ function initMap(bounds, setStartCoord, setIntermediateCoord, setEndCoord, selec
     }, {
         text: translate.tr('create_poi'),
         callback: function (e) {
-            alert("FIXME: Create POI" + e.latlng.lat + "," + e.latlng.lng);
+            POIDialog("Create POI at " + e.latlng.lat + "," + e.latlng.lng);
         },
         index: 11
     }, {
@@ -494,4 +494,200 @@ function addOverlayLayer(name, id, minzoom) {
     
     var layers_menu = document.getElementById('layer_menu');
     layers_menu.appendChild(link);
+}
+
+function POIDialog(title) {
+    var iconSelect = new IconSelect("poi-dialog", 
+        {'selectedIconWidth':48,
+        'selectedIconHeight':48,
+        'selectedBoxPadding':1,
+        'iconsWidth':23,
+        'iconsHeight':23,
+        'boxIconSpace':1,
+        'vectoralIconNumber':14,
+        'horizontalIconNumber':14});
+
+    var icons = [];
+    icons.push({'iconFilePath':'images/icons/abseiling.svg',                  'iconValue':'1'});
+    icons.push({'iconFilePath':'images/icons/accounting.svg',                 'iconValue':'2'});
+    icons.push({'iconFilePath':'images/icons/airport.svg',                    'iconValue':'3'});
+    icons.push({'iconFilePath':'images/icons/amusement-park.svg',             'iconValue':'4'});
+    icons.push({'iconFilePath':'images/icons/aquarium.svg',                   'iconValue':'5'});
+    icons.push({'iconFilePath':'images/icons/archery.svg',                    'iconValue':'6'});
+    icons.push({'iconFilePath':'images/icons/art-gallery.svg',                'iconValue':'7'});
+    icons.push({'iconFilePath':'images/icons/assistive-listening-system.svg', 'iconValue':'8'});
+    icons.push({'iconFilePath':'images/icons/atm.svg',                        'iconValue':'9'});
+    icons.push({'iconFilePath':'images/icons/audio-description.svg',          'iconValue':'10'});
+    icons.push({'iconFilePath':'images/icons/bakery.svg',                     'iconValue':'11'});
+    icons.push({'iconFilePath':'images/icons/bank.svg',                       'iconValue':'12'});
+    icons.push({'iconFilePath':'images/icons/bar.svg',                        'iconValue':'13'});
+    icons.push({'iconFilePath':'images/icons/baseball.svg',                   'iconValue':'14'});
+    icons.push({'iconFilePath':'images/icons/beauty-salon.svg',               'iconValue':'15'});
+    icons.push({'iconFilePath':'images/icons/bicycle-store.svg',              'iconValue':'16'});
+    icons.push({'iconFilePath':'images/icons/boating.svg',                    'iconValue':'18'});
+    icons.push({'iconFilePath':'images/icons/boat-ramp.svg',                  'iconValue':'19'});
+    icons.push({'iconFilePath':'images/icons/boat-tour.svg',                  'iconValue':'20'});
+    icons.push({'iconFilePath':'images/icons/book-store.svg',                 'iconValue':'21'});
+    icons.push({'iconFilePath':'images/icons/bowling-alley.svg',              'iconValue':'22'});
+    icons.push({'iconFilePath':'images/icons/braille.svg',                    'iconValue':'23'});
+    icons.push({'iconFilePath':'images/icons/bus-station.svg',                'iconValue':'24'});
+    icons.push({'iconFilePath':'images/icons/cafe.svg',                       'iconValue':'25'});
+    icons.push({'iconFilePath':'images/icons/campground.svg',                 'iconValue':'26'});
+    icons.push({'iconFilePath':'images/icons/canoe.svg',                      'iconValue':'27'});
+    icons.push({'iconFilePath':'images/icons/car-dealer.svg',                 'iconValue':'28'});
+    icons.push({'iconFilePath':'images/icons/car-rental.svg',                 'iconValue':'29'});
+    icons.push({'iconFilePath':'images/icons/car-repair.svg',                 'iconValue':'30'});
+    icons.push({'iconFilePath':'images/icons/car-wash.svg',                   'iconValue':'31'});
+    icons.push({'iconFilePath':'images/icons/casino.svg',                     'iconValue':'32'});
+    icons.push({'iconFilePath':'images/icons/cemetery.svg',                   'iconValue':'33'});
+    icons.push({'iconFilePath':'images/icons/chairlift.svg',                  'iconValue':'34'});
+    icons.push({'iconFilePath':'images/icons/church.svg',                     'iconValue':'35'});
+    icons.push({'iconFilePath':'images/icons/circle.svg',                     'iconValue':'36'});
+    icons.push({'iconFilePath':'images/icons/city-hall.svg',                  'iconValue':'37'});
+    icons.push({'iconFilePath':'images/icons/climbing.svg',                   'iconValue':'38'});
+    icons.push({'iconFilePath':'images/icons/closed-captioning.svg',          'iconValue':'39'});
+    icons.push({'iconFilePath':'images/icons/clothing-store.svg',             'iconValue':'40'});
+    icons.push({'iconFilePath':'images/icons/compass.svg',                    'iconValue':'41'});
+    icons.push({'iconFilePath':'images/icons/convenience-store.svg',          'iconValue':'42'});
+    icons.push({'iconFilePath':'images/icons/courthouse.svg',                 'iconValue':'43'});
+    icons.push({'iconFilePath':'images/icons/cross-country-skiing.svg',       'iconValue':'44'});
+    icons.push({'iconFilePath':'images/icons/crosshairs.svg',                 'iconValue':'45'});
+    icons.push({'iconFilePath':'images/icons/dentist.svg',                    'iconValue':'46'});
+    icons.push({'iconFilePath':'images/icons/department-store.svg',           'iconValue':'47'});
+    icons.push({'iconFilePath':'images/icons/diving.svg',                     'iconValue':'48'});
+    icons.push({'iconFilePath':'images/icons/doctor.svg',                     'iconValue':'49'});
+    icons.push({'iconFilePath':'images/icons/electrician.svg',                'iconValue':'50'});
+    icons.push({'iconFilePath':'images/icons/electronics-store.svg',          'iconValue':'51'});
+    icons.push({'iconFilePath':'images/icons/embassy.svg',                    'iconValue':'52'});
+    icons.push({'iconFilePath':'images/icons/expand.svg',                     'iconValue':'53'});
+    icons.push({'iconFilePath':'images/icons/female.svg',                     'iconValue':'54'});
+    icons.push({'iconFilePath':'images/icons/finance.svg',                    'iconValue':'55'});
+    icons.push({'iconFilePath':'images/icons/fire-station.svg',               'iconValue':'56'});
+    icons.push({'iconFilePath':'images/icons/fish-cleaning.svg',              'iconValue':'57'});
+    icons.push({'iconFilePath':'images/icons/fishing-pier.svg',               'iconValue':'59'});
+    icons.push({'iconFilePath':'images/icons/florist.svg',                    'iconValue':'60'});
+    icons.push({'iconFilePath':'images/icons/food.svg',                       'iconValue':'61'});
+    icons.push({'iconFilePath':'images/icons/fullscreen.svg',                 'iconValue':'62'});
+    icons.push({'iconFilePath':'images/icons/funeral-home.svg',               'iconValue':'63'});
+    icons.push({'iconFilePath':'images/icons/furniture-store.svg',            'iconValue':'64'});
+    icons.push({'iconFilePath':'images/icons/gas-station.svg',                'iconValue':'65'});
+    icons.push({'iconFilePath':'images/icons/general-contractor.svg',         'iconValue':'66'});
+    icons.push({'iconFilePath':'images/icons/grocery-or-supermarket.svg',     'iconValue':'68'});
+    icons.push({'iconFilePath':'images/icons/gym.svg',                        'iconValue':'69'});
+    icons.push({'iconFilePath':'images/icons/hair-care.svg',                  'iconValue':'70'});
+    icons.push({'iconFilePath':'images/icons/hang-gliding.svg',               'iconValue':'71'});
+    icons.push({'iconFilePath':'images/icons/hardware-store.svg',             'iconValue':'72'});
+    icons.push({'iconFilePath':'images/icons/health.svg',                     'iconValue':'73'});
+    icons.push({'iconFilePath':'images/icons/hindu-temple.svg',               'iconValue':'74'});
+    icons.push({'iconFilePath':'images/icons/hospital.svg',                   'iconValue':'76'});
+    icons.push({'iconFilePath':'images/icons/ice-fishing.svg',                'iconValue':'77'});
+    icons.push({'iconFilePath':'images/icons/ice-skating.svg',                'iconValue':'78'});
+    icons.push({'iconFilePath':'images/icons/inline-skating.svg',             'iconValue':'79'});
+    icons.push({'iconFilePath':'images/icons/insurance-agency.svg',           'iconValue':'80'});
+    icons.push({'iconFilePath':'images/icons/jet-skiing.svg',                 'iconValue':'81'});
+    icons.push({'iconFilePath':'images/icons/jewelry-store.svg',              'iconValue':'82'});
+    icons.push({'iconFilePath':'images/icons/kayaking.svg',                   'iconValue':'83'});
+    icons.push({'iconFilePath':'images/icons/laundry.svg',                    'iconValue':'84'});
+    icons.push({'iconFilePath':'images/icons/lawyer.svg',                     'iconValue':'85'});
+    icons.push({'iconFilePath':'images/icons/library.svg',                    'iconValue':'86'});
+    icons.push({'iconFilePath':'images/icons/liquor-store.svg',               'iconValue':'87'});
+    icons.push({'iconFilePath':'images/icons/local-government.svg',           'iconValue':'88'});
+    icons.push({'iconFilePath':'images/icons/location-arrow.svg',             'iconValue':'89'});
+    icons.push({'iconFilePath':'images/icons/locksmith.svg',                  'iconValue':'90'});
+    icons.push({'iconFilePath':'images/icons/lodging.svg',                    'iconValue':'91'});
+    icons.push({'iconFilePath':'images/icons/low-vision-access.svg',          'iconValue':'92'});
+    icons.push({'iconFilePath':'images/icons/male.svg',                       'iconValue':'93'});
+    icons.push({'iconFilePath':'images/icons/map-pin.svg',                    'iconValue':'94'});
+    icons.push({'iconFilePath':'images/icons/marina.svg',                     'iconValue':'95'});
+    icons.push({'iconFilePath':'images/icons/mosque.svg',                     'iconValue':'96'});
+    icons.push({'iconFilePath':'images/icons/movie-rental.svg',               'iconValue':'98'});
+    icons.push({'iconFilePath':'images/icons/movie-theater.svg',              'iconValue':'99'});
+    icons.push({'iconFilePath':'images/icons/moving-company.svg',             'iconValue':'101'})
+    icons.push({'iconFilePath':'images/icons/museum.svg',                     'iconValue':'102'})
+    icons.push({'iconFilePath':'images/icons/natural-feature.svg',            'iconValue':'103'})
+    icons.push({'iconFilePath':'images/icons/night-club.svg',                 'iconValue':'104'})
+    icons.push({'iconFilePath':'images/icons/open-captioning.svg',            'iconValue':'105'})
+    icons.push({'iconFilePath':'images/icons/painter.svg',                    'iconValue':'106'})
+    icons.push({'iconFilePath':'images/icons/park.svg',                       'iconValue':'107'})
+    icons.push({'iconFilePath':'images/icons/parking.svg',                    'iconValue':'108'})
+    icons.push({'iconFilePath':'images/icons/pet-store.svg',                  'iconValue':'109'})
+    icons.push({'iconFilePath':'images/icons/pharmacy.svg',                   'iconValue':'110'})
+    icons.push({'iconFilePath':'images/icons/physiotherapist.svg',            'iconValue':'111'})
+    icons.push({'iconFilePath':'images/icons/place-of-worship.svg',           'iconValue':'112'})
+    icons.push({'iconFilePath':'images/icons/playground.svg',                 'iconValue':'113'})
+    icons.push({'iconFilePath':'images/icons/plumber.svg',                    'iconValue':'114'})
+    icons.push({'iconFilePath':'images/icons/point-of-interest.svg',          'iconValue':'115'})
+    icons.push({'iconFilePath':'images/icons/police.svg',                     'iconValue':'116'})
+    icons.push({'iconFilePath':'images/icons/political.svg',                  'iconValue':'117'})
+    icons.push({'iconFilePath':'images/icons/postal-code.svg',                'iconValue':'118'})
+    icons.push({'iconFilePath':'images/icons/postal-code-prefix.svg',         'iconValue':'119'})
+    icons.push({'iconFilePath':'images/icons/post-box.svg',                   'iconValue':'120'})
+    icons.push({'iconFilePath':'images/icons/post-office.svg',                'iconValue':'121'})
+    icons.push({'iconFilePath':'images/icons/rafting.svg',                    'iconValue':'122'})
+    icons.push({'iconFilePath':'images/icons/real-estate-agency.svg',         'iconValue':'123'})
+    icons.push({'iconFilePath':'images/icons/restaurant.svg',                 'iconValue':'124'})
+    icons.push({'iconFilePath':'images/icons/roofing-contractor.svg',         'iconValue':'125'})
+    icons.push({'iconFilePath':'images/icons/route.svg',                      'iconValue':'126'})
+    icons.push({'iconFilePath':'images/icons/route-pin.svg',                  'iconValue':'127'})
+    icons.push({'iconFilePath':'images/icons/rv-park.svg',                    'iconValue':'128'})
+    icons.push({'iconFilePath':'images/icons/sailing.svg',                    'iconValue':'129'})
+    icons.push({'iconFilePath':'images/icons/school.svg',                     'iconValue':'130'})
+    icons.push({'iconFilePath':'images/icons/scuba-diving.svg',               'iconValue':'131'})
+    icons.push({'iconFilePath':'images/icons/search.svg',                     'iconValue':'132'})
+    icons.push({'iconFilePath':'images/icons/sheild.svg',                     'iconValue':'133'})
+    icons.push({'iconFilePath':'images/icons/shopping-mall.svg',              'iconValue':'134'})
+    icons.push({'iconFilePath':'images/icons/sign-language.svg',              'iconValue':'135'})
+    icons.push({'iconFilePath':'images/icons/skateboarding.svg',              'iconValue':'136'})
+    icons.push({'iconFilePath':'images/icons/skiing.svg',                     'iconValue':'137'})
+    icons.push({'iconFilePath':'images/icons/ski-jumping.svg',                'iconValue':'138'})
+    icons.push({'iconFilePath':'images/icons/sledding.svg',                   'iconValue':'139'})
+    icons.push({'iconFilePath':'images/icons/snow.svg',                       'iconValue':'140'})
+    icons.push({'iconFilePath':'images/icons/snowboarding.svg',               'iconValue':'151'})
+    icons.push({'iconFilePath':'images/icons/snowmobile.svg',                 'iconValue':'152'})
+    icons.push({'iconFilePath':'images/icons/snow-shoeing.svg',               'iconValue':'153'})
+    icons.push({'iconFilePath':'images/icons/spa.svg',                        'iconValue':'154'})
+    icons.push({'iconFilePath':'images/icons/square.svg',                     'iconValue':'155'})
+    icons.push({'iconFilePath':'images/icons/square-pin.svg',                 'iconValue':'156'})
+    icons.push({'iconFilePath':'images/icons/square-rounded.svg',             'iconValue':'157'})
+    icons.push({'iconFilePath':'images/icons/stadium.svg',                    'iconValue':'158'})
+    icons.push({'iconFilePath':'images/icons/storage.svg',                    'iconValue':'159'})
+    icons.push({'iconFilePath':'images/icons/store.svg',                      'iconValue':'160'})
+    icons.push({'iconFilePath':'images/icons/subway-station.svg',             'iconValue':'161'})
+    icons.push({'iconFilePath':'images/icons/surfing.svg',                    'iconValue':'162'})
+    icons.push({'iconFilePath':'images/icons/swimming.svg',                   'iconValue':'163'})
+    icons.push({'iconFilePath':'images/icons/synagogue.svg',                  'iconValue':'164'})
+    icons.push({'iconFilePath':'images/icons/taxi-stand.svg',                 'iconValue':'165'})
+    icons.push({'iconFilePath':'images/icons/tennis.svg',                     'iconValue':'166'})
+    icons.push({'iconFilePath':'images/icons/toilet.svg',                     'iconValue':'167'})
+    icons.push({'iconFilePath':'images/icons/train-station.svg',              'iconValue':'169'})
+    icons.push({'iconFilePath':'images/icons/transit-station.svg',            'iconValue':'170'})
+    icons.push({'iconFilePath':'images/icons/travel-agency.svg',              'iconValue':'171'})
+    icons.push({'iconFilePath':'images/icons/unisex.svg',                     'iconValue':'172'})
+    icons.push({'iconFilePath':'images/icons/university.svg',                 'iconValue':'173'})
+    icons.push({'iconFilePath':'images/icons/veterinary-care.svg',            'iconValue':'174'})
+    icons.push({'iconFilePath':'images/icons/volume-control-telephone.svg',   'iconValue':'176'})
+    icons.push({'iconFilePath':'images/icons/waterskiing.svg',                'iconValue':'178'})
+    icons.push({'iconFilePath':'images/icons/whale-watching.svg',             'iconValue':'179'})
+    icons.push({'iconFilePath':'images/icons/wheelchair.svg',                 'iconValue':'180'})
+    icons.push({'iconFilePath':'images/icons/wind-surfing.svg',               'iconValue':'181'})
+    icons.push({'iconFilePath':'images/icons/zoo.svg',                        'iconValue':'182'})
+    icons.push({'iconFilePath':'images/icons/zoom-in.svg',                    'iconValue':'183'})
+    icons.push({'iconFilePath':'images/icons/zoom-in-alt.svg',                'iconValue':'184'})
+    icons.push({'iconFilePath':'images/icons/zoom-out.svg',                   'iconValue':'185'})
+    icons.push({'iconFilePath':'images/icons/zoom-out-alt.svg',               'iconValue':'186'})
+
+    iconSelect.refresh(icons);
+
+    $("#poi-dialog").dialog({
+      resizable: false,
+      title: title,
+      height: 500,
+      width: 500,
+      modal: true,
+      buttons: {
+        Ok: function() {
+          $(this).dialog( "close" );
+        }
+      }
+    });
 }
